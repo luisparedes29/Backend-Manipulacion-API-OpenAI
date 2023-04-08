80f0e23be38e028b2a3379dd023d6ad171b8d93f
Atilio <atiliojg1@gmail.com>
2023-04-08 19:34:35 -0400

    Se corrigió la escritura de algunas partes del código. Se creó un archivo(statusResponse/res.js) desde donde se exportan las funciones para controlar las llamadas exitosas y fallidas a la API. Se borró la variable nombre y se incluyeron las variables para la foto de perfil, preferencias de tema y sonido, las cuales van a tener un valor predefinido al momento de crear la cuenta. Corregidos algunos fallos. Ahora la tabla donde va la información de los usuarios se crea sola.

--- Backend/src/routes/controllers/JWTController.js
+++ Backend/src/routes/controllers/JWTController.js
@@ -4,11 +4,10 @@ const jwt = require('jsonwebtoken');
 module.exports.JWTController = {
     createToken(payload) {
         const expirationTime = new Date(Date.now() + (10 * 60 * 1000)); // La duracion es de 10 minutos en milisegundos
-        console.log("Creando el token...");
         const accessToken = jwt.sign({
             exp: Math.floor(expirationTime / 1000), // Aqui los milisegundos se transforman a 10 minutos al dividirlos entre 1000
             data: payload
         }, process.env.secret);
-        console.log("Token creado:", accessToken);
+        return accessToken;
     },
 }
--- Backend/src/routes/controllers/registroController.js
+++ Backend/src/routes/controllers/registroController.js
@@ -3,6 +3,10 @@ const { usuario } = require('../../models/user');
 const { conexionDb } = require("../../dbconfig");
 const { usuarioController } = require('./usuarioController');
 const { JWTController } = require('./JWTController');
+const { resError, resSuccess } = require('../../../statusResponse/res')
+
+
+
 
 module.exports.registroController = {
     async registrarUsuario(req, res) {
@@ -11,44 +15,48 @@ module.exports.registroController = {
             const usuarioExistente = await usuarioController.getUsuarioByCorreo(req.body.correo);
             const usuarioExistentePorNombreUsuario = await usuarioController.getUsuarioByUsername(req.body.username);
             if (usuarioExistente) {
-                let mensaje = '';
                 if (usuarioExistente.correo === req.body.correo) {
-                    mensaje += 'Ya existe una cuenta con este correo. ';
+                    return resError(req, res, 'Ya existe una cuenta con este correo.', 409)
                 }
-                return res.status(400).json({ errors: { message: mensaje } });
             }
             if (usuarioExistentePorNombreUsuario) {
-                let mensaje = 'Ya existe una cuenta con este nombre de usuario.';
-                return res.status(400).json({ errors: { message: mensaje } });
+                return resError(res, res, 'Ya existe un usuario con este mismo user.', 409)
             }
             const hashedPassword = await bcrypt.hash(req.body.password, 10);
             const nuevoUsuario = await usuario.create({
-                nombre: req.body.nombre,
                 correo: req.body.correo,
                 username: req.body.username,
                 password: hashedPassword,
+                fotoPerfil: 'https://i.pinimg.com/564x/fc/a3/da/fca3dae7dcd86c63e25458b30742f709.jpg',
+                estilosPref: 1,
+                sonidoPref: 1
             });
             const token = JWTController.createToken({ correo: nuevoUsuario.correo });
-            return res.status(201).json({ usuario: nuevoUsuario, token: token, message: 'La cuenta se ha creado exitosamente' });
+            console.log(nuevoUsuario)
+            console.log('Hola, aqui esta el token:', token)
+            return resSuccess(req, res, 'La cuenta se ha creado exitosamente.', 201)
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({ errors: { message: 'Ocurrió un error al registrar al usuario' } });
+            return resError(req, res, 'Ocurrió un error al registrar al usuario.', 500)
         }
     },
 
     async inicioSesion(req, res) {
-        await conexionDb();
-        const usuarioExistente = await usuarioController.getUsuarioByCorreo(req.body.correo);
-        if (!usuarioExistente) {
-            let mensaje = 'El usuario no está registrado. Por favor, regístrese para poder acceder.'
-            return res.status(200).json(mensaje)
-        };
-
-        if (await bcrypt.compare(req.body.password, usuarioExistente.password)) {
-            const token = JWTController.createToken({ correo: usuarioExistente.correo })
-            res.json({ usuarioExistente, token });
+        try {
+            await conexionDb();
+            const usuarioExistente = await usuarioController.getUsuarioByCorreo(req.body.correo);
+            if (!usuarioExistente) {
+                return resError(req, res, 'El usuario no está registrado. Por favor, regístrese para poder acceder.', 401);
+            };
+            if (await bcrypt.compare(req.body.password, usuarioExistente.password)) {
+                const token = JWTController.createToken({ correo: usuarioExistente.correo });
+                console.log('Hola, aqui esta el token:', token)
+                return resSuccess(req, res, `El inicio de sesión ha sido exitoso. Bienvenido ${usuarioExistente.username}`, 200);
+            }
+            else {
+                return resError(req, res, 'La contraseña es incorrecta. Por favor inténtelo de nuevo', 401);
+            }
+        } catch (error) {
+            return resError(req, res, 'Ha ocurrido un error en el inicio de sesión. Por favor inténtelo de nuevo más tarde.', 500);
         }
-        else
-            res.status(401).json({ errors: { message: 'Datos incorrectos. Verifique' } })
     }
 }
