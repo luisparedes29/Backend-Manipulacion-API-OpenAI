30cc02b33df85d4df795db05f98d6bfaea11c7d3
luisparedes29 <luisparedescrv@gmail.com>
2023-04-15 01:56:53 -0400

    Manejo de imagenes casi listo, solo falta enviar la URL a la BD

---
+++ Backend/src/routes/controllers/imagenController.js
@@ -0,0 +1,56 @@
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const fs = require('fs');
+const sharp = require('sharp');
+
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.KEY_CLOUD,
+    api_secret: process.env.KEY_SECRET,
+});
+
+const storage = multer.diskStorage({
+    destination: './',
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 },
+});
+
+const cargarImagen = (req, res) => {
+    upload.fields([{ name: 'image', maxCount: 1 }])(req, res, async (err) => {
+        if (err) {
+            return res.status(400).json({ error: err.message });
+        }
+
+        try {
+            // Retrieve uploaded files from request object
+            const image = req.files.image ? req.files.image[0] : undefined;
+
+            if (image) {
+                const response = await cloudinary.uploader.upload(image.path, {
+                    folder: 'images',
+                });
+                res.status(201).json({
+                    image: { public_id: response.public_id, url: response.secure_url },
+                });
+                fs.unlinkSync(image.path);
+            } else {
+                res.status(400).json({ error: 'No se envió ningún archivo' });
+            }
+        } catch {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+};
+
+
+module.exports.imagenController = {
+    cargarImagen,
+};
