cd61f6921fef02bca467a5c59973844b47def8b1
Ricardoandreslh <ricardoandresloaiza@gmail.com>
2023-04-18 21:17:34 -0500

    Eliminado el controlador de personalización. Documentacion de Swagger completa!

--- Backend/src/routes/respuesta.js
+++ Backend/src/routes/respuesta.js
@@ -5,12 +5,16 @@ const { registroController } = require("./controllers/registroController");
 const { loginValidator } = require("../validators/loginValidator");
 const { registroValidator } = require("../validators/registroValidator");
 const { imagenController}= require("./controllers/imagenController")
-const { actualizarPreferencias } = require('./controllers/personalizacionController')
 const authToken = require("./controllers/JWTAuthController");
 
 /**
  * @swagger
- * components:
+ *  components:
+ *  securitySchemes: 
+ *      bearerAuth: 
+ *           type: http 
+ *           scheme: bearer
+ *           bearerFormat: JWT
  *  schemas:
  *      usuario:
  *          type: object
@@ -30,8 +34,8 @@ const authToken = require("./controllers/JWTAuthController");
  *              - password
  *          example:
  *              correo: correoejemplo@gmail.com
- *              username: Nombre Random
- *              password: ContraseñaRandom
+ *              username: Nombrerandom
+ *              password: C0ntras3na
  *      login:
  *          type: object
  *          properties:
@@ -46,8 +50,8 @@ const authToken = require("./controllers/JWTAuthController");
  *              - password
  *          example:
  *              correo: correoejemplo@gmail.com
- *              password: ContraseñaRandom
- *      /:
+ *              password: C0ntras3na
+ *      respuesta:
  *          type: object
  *          properties:
  *              pregunta:
@@ -57,22 +61,49 @@ const authToken = require("./controllers/JWTAuthController");
  *              - pregunta
  *          example:
  *              pregunta: Como te llamas?
+ *      upload:
+ *          type: object
+ *          properties:
+ *              image:
+ *                  type: string
+ *                  description: imagen del usuario
+ *              username:
+ *                  type: string
+ *                  description: username del usuario
+ *          required:
+ *              - image
+ *              - username
+ *          example:
+ *              image: https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png
+ *              username: Nombrerandom
+ *      getImage:
+ *          type: object
+ *          properties:
+ *              correo:
+ *                  type: string
+ *                  description: correo del usuario
+ *          required:
+ *              - correo
+ *          example:
+ *              correo: correoejemplo@gmail.com
  */
 
 
 /**
  * @swagger
- * /api//:
+ * /:
  *  post:
  *      summary: Pregunta del usuario
  *      tags: [Pregunta]
+ *      security:
+ *       - bearerAuth: []
  *      requestBody:
  *          required: true
  *          content:
  *              application/json:
  *                  schema:
  *                      type: object
- *                      $ref: '#/components/schemas//'
+ *                      $ref: '#/components/schemas/respuesta'
  *      responses:
  *          200:
  *              description: Pregunta exitosa.
@@ -88,9 +119,9 @@ router.post("/", authToken, async (req, res) => {
 
 /**
  * @swagger
- * /api/registrar_usuario:
+ * /registrar_usuario:
  *  post:
- *      summary: Registrar Usuarionode
+ *      summary: Registrar Usuario
  *      tags: [Registrar]
  *      requestBody:
  *          required: true
@@ -113,7 +144,7 @@ router.post('/registrar_usuario', registroValidator, registroController.registra
 
 /**
  * @swagger
- * /api/login:
+ * /login:
  *  post:
  *      summary: Entrar a la Sesión
  *      tags: [Login]
@@ -139,7 +170,7 @@ router.post('/login', loginValidator, registroController.inicioSesion);
 
 /**
  * @swagger
- * /api/logout:
+ * /logout:
  *  post:
  *      summary: Salir de la Sesión
  *      tags: [Logout]
@@ -154,10 +185,51 @@ router.post('/login', loginValidator, registroController.inicioSesion);
 
 router.post('/logout', registroController.cerrarSesion);
 
+
+/**
+ * @swagger
+ * /upload:
+ *  post:
+ *      summary: Foto de perfil del Usuario
+ *      tags: [Foto]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/upload'
+ *      responses:
+ *          400:
+ *              description: No se envió ningún archivo.
+ *          500:
+ *              description: Internal Server Error.
+ */
+
 router.post('/upload', imagenController.cargarImagen);
 
-router.post('/getImage', imagenController.getImagen);
 
-router.put('/personalizacion', actualizarPreferencias)
+
+/**
+ * @swagger
+ * /getImage:
+ *  post:
+ *      summary: Get Image
+ *      tags: [Obtener]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/getImage'
+ *      responses:
+ *          200:
+ *              description: La imagen ha sido encontrada!.
+ *          404:
+ *              description: Usuario no encontrado.
+ */
+
+router.post('/getImage', imagenController.getImagen);
 
 module.exports = router;
